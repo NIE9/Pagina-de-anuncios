75e9855e8e6e421ff06ef6ee1112b7c695fdf69e
NIE9 <o.thiagonienov@gmail.com>
2022-06-07 17:56:20 -0300

    php banco e endpoint

---
+++ Banco/bancos.php
@@ -0,0 +1,32 @@
+<?php
+define("HOST","localhost");
+define("DB_NAME","PaginaDeAnuncios");
+define("DB_USER","root");
+define("DB_PASSWORD","");
+
+class banco{
+    private $conexao;
+
+    public function __construct(){
+        $this->conexao = new mysqli(HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        if ($this->conexao->connect_errno) {
+            echo "Falha ao conectar no MySQL: " . $this->conexao->connect_error;
+        }
+    }
+
+    public function consultar(string $query){
+        $resultado = $this->conexao->query($query);
+        $dados = array();
+        while($linha = $resultado->fetch_assoc()){
+            $dados[] = $linha;
+        }
+        return $dados;
+    }
+
+    public function manipular(string $query){
+        $resultado = $this->conexao->query($query);
+        return $resultado;
+    }
+}
+
+?>
--- Banco/infodoproduto.sql
+++ Banco/infodoproduto.sql
@@ -1,3 +1,5 @@
+-- noinspection SqlDialectInspectionForFile
+
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
