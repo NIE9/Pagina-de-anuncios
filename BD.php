51a36c717b3b36ead80d8e9c5f47878280a1d479
NIE9 <o.thiagonienov@gmail.com>
2022-06-21 17:44:11 -0300

    primeiro commit

---
+++ BD.php
@@ -0,0 +1,7 @@
+<?php
+$mysqli = new mysqli("locallhost", "root", "", "contatos");
+
+if ($mysqli->connect_errno) {
+    echo "falhou ao conectar ao MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_errno;
+}
+php?>
